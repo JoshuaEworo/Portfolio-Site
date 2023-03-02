5fc7afc7c5acfcf58d691cf8b2dd1bc5f6ef8e94
Joshua Eworo <72352089+JoshuaEworo@users.noreply.github.com>
2023-03-02 16:50:51 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -79,16 +79,16 @@ export default function Home() {
 
   return (
     <div className="pageWrapper">
-      <Head>
-        <title>Joshua Eworo</title>
-        <meta name="description" content="Portfolio of Joshua Eworo, Software Engineer based in Texas." />
-        <meta property="og:title" content="Joshua Eworo - Software Engineer Portfolio"/>
-        <meta property="og:description" content="Explore Joshua Eworo's software engineering portfolio."/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://joshuaeworo.dev"/>
-        <meta property="og:image" content="https://i.ibb.co/KVqTSyj/E9-D2-B63-C-39-B9-4-C6-B-B6-A9-1944-F8-B9-C8-C3.jpg"/>
-        <meta property="og:site_name" content="Joshua Eworo"/>
-      <Head/>
+        <Head>
+            <title>Joshua Eworo</title>
+            <meta name="description" content="Portfolio of Joshua Eworo, Software Engineer based in Texas." />\
+            <meta property="og:title" content="Joshua Eworo - Software Engineer Portfolio"/>
+            <meta property="og:description" content="Explore Joshua Eworo's software engineering portfolio."/>
+            <meta property="og:type" content="website"/>
+            <meta property="og:url" content="https://joshuaeworo.dev"/>
+            <meta property="og:image" content="https://i.ibb.co/KVqTSyj/E9-D2-B63-C-39-B9-4-C6-B-B6-A9-1944-F8-B9-C8-C3.jpg"/>
+            <meta property="og:site_name" content="Joshua Eworo"/>`
+        <Head/>
 
       <Navbar />
       <main className=''>
